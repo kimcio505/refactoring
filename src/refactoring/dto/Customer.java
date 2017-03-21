daac26ae55031fdffe3123002be0f05cde655f57
jwSHIN <kimcio505@naver.com>
2017-03-21 09:30:24 +0900

    2017-03-21 09:30 임시변수 thisAmount를 메서드 호출기법(each.getCharge()로 전환

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -30,15 +30,14 @@ public class Customer {
       int frequentRanterPoints=0;//적립포인트
       StringBuilder sb = new StringBuilder();
       sb.append(getName() + "고객님의 대여기록\n");
-      for(Rental each : rentals){
-         double thisAmount = each.getcharge(); 
+      for(Rental each : rentals){ 
          frequentRanterPoints++;
          if(each.getMovie().getPriceCode()==Movie.NEW_RELEASE && each.getDaysRented()>1){
             frequentRanterPoints++;
          }
-         sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), thisAmount));
+         sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getcharge()));
          
-         totalAmount += thisAmount;
+         totalAmount += each.getcharge(); 
       }//for loop
       
       sb.append(String.format("누적대여료 : %s%n적립 포인트 : %s%n", totalAmount, frequentRanterPoints));
